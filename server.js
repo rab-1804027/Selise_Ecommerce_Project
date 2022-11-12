8ce675760bccd4f20d9d9d1cc27a555514ab5efb
arifulanik <khananik12122@gmail.com>
2022-11-12 17:36:50 +0600

    heroku hosting

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Final-Ecommerce'));
+app.use(express.static(__dirname + '/dist/final-ecommerce'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Final-Ecommerce/'}),
+    res.sendFile(path.join(__dirname + '/dist/final-ecommerce/index.html')),
 );
 
 // Start the app by listening on the default Heroku port
